709d687011e2c206196dc935d2a695b27a37e73d
damonryon <damon.ryon@gmail.com>
2024-04-20 14:07:42 -0700

    03 solved

--- problems/02-recursive-sort.js
+++ problems/02-recursive-sort.js
@@ -10,9 +10,18 @@
 */
 
 function sort(nums) {
-    // Your code here 
+    if(nums.length === 0) return []
+    let smallNum = Infinity
+    for(let el of nums){
+        if(el < smallNum) smallNum = el
+    }
+    return [smallNum, ...sort(nums.filter(el => el !== smallNum))]
 }
 
+console.log(sort([])) // prints []
+console.log(sort([9])) // prints [9]
+console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
+console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
     module.exports = sort;
